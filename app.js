ff4edfc9fd6290b043f62879550b9636dfb64780
crashingstar <junhao12345@hotmail.com>
2025-03-15 14:56:34 +0800

    theme switcher functionality

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ const sidebarToggle = () => {
 
 const sidebarTrigger = document.getElementById("main-header__sidebar-toggle");
 sidebarTrigger.addEventListener("click", sidebarToggle);
+
+// Theme switcher function
+const switchTheme = () => {
+  // Get root element and data-theme value
+  const rootElem = document.documentElement;
+  let dataTheme = rootElem.getAttribute("data-theme"),
+    newTheme;
+
+  newTheme = dataTheme === "light" ? "dark" : "light";
+
+  // Set the new HTML attribute
+  rootElem.setAttribute("data-theme", newTheme);
+
+  // Set the new local storage item
+  localStorage.setItem("theme", newTheme);
+};
+
+// Add the event listener for the theme switcher
+document
+  .querySelector("#sidebar__theme-switcher")
+  .addEventListener("click", switchTheme);
